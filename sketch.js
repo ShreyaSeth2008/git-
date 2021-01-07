1535627a5cbc7827685ecff3742ef51907c4e6ce
ShreyaSeth2008 <shreyaseth2008@gmail.com>
2021-01-07 15:49:30 +0530

    created new box object "box30"

--- sketch.js
+++ sketch.js
@@ -25,6 +25,7 @@ function setup(){
     log1 = new Log(810,260,300, PI/2);
 
     box3 = new Box(700,240,70,70);
+    box30=new Box(466, 899, 477, 958)
     box4 = new Box(920,240,70,70);
     pig3 = new Pig(810, 220);
 
